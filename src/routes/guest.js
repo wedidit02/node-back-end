9c83808fa1a21e4b8a51ef09a53a0daffccc1002
prinako <nyankonton122@gmail.com>
2021-09-21 19:23:23 -0300

    fixe problems with cart router

--- src/routes/guest.js
+++ src/routes/guest.js
@@ -9,7 +9,7 @@ router.get("", checkNotAuthenticated, async (req, res) => {
   });
 });
 
-router.get("/cart", (req, res) => {
+router.get("/cart", checkNotAuthenticated, (req, res) => {
     res.render("cart",{ userId: null, products: [], title: "Cart" })
 });
 
